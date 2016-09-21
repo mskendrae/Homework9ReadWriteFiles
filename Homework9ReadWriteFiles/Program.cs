bff6ed62edc9ab2aae4b6711797832496ebe3f15
Kendra Egert <mskendrae@gmail.com>
2016-09-21 15:00:09 -0400

    first commit

---
+++ Homework9ReadWriteFiles/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework9ReadWriteFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Read and print the first line from the file only.
+            // Read and print the entire text file.
+            // Read and print the entire text file and capitalize all lowercase vowels.
+
+            //Program will ask the use to choose which of the above criteria to run.
+
+            //Menu of the program
+            Console.WriteLine("1. Read and Print the first line from the File to Console.");
+            Console.WriteLine("2. Read and Print the entire text file to Console. ");
+            Console.WriteLine("3. Read and Print the entire text file with all lowercase vowels Captilized.");
+
+            Console.WriteLine("Please choose the option you'd like to run.");
+            int userChoice = int.Parse(Console.ReadLine());
+
+            //First Choice 
+            switch();
+
+            case: 1
+
+
+
+
+
+
+        }
+    }
+}
